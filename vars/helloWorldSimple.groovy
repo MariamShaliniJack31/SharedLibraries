778e3d10df2add2c028eba807cad3fe633e4d8fe
MariamShaliniJack31 <75356355+MariamShaliniJack31@users.noreply.github.com>
2022-12-19 18:50:23 -0500

    Update helloWorldSimple.groovy

--- vars/helloWorldSimple.groovy
+++ vars/helloWorldSimple.groovy
@@ -1,3 +1,7 @@
 def call(String name, String dayOfWeek) {
   echo "Hello World ${name}. It is ${dayOfWeek}."
 }
+
+def calling(String name, String dayOfWeek) {
+  echo "Hello Calling World ${name}. It is ${dayOfWeek}."
+}
